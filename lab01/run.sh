934e835b36e11506c1e3bb40275747980b07e861
GrachevaNN <grann72@mail.ru>
2021-06-30 18:01:39 +0300

    30.06.2021_18:01:39,81

---
+++ lab01/run.sh
@@ -0,0 +1,30 @@
+git pull
+
+echo ''
+echo '------- Compiling main.c into main.o: mpicxx -O3 -c main.c ------'
+mpicxx -O3 -c main.c
+echo '-------- mpicxx -o myapp main.o -----'
+mpicxx -o myapp main.o
+echo '-------------'
+
+echo '------- Starting myapp in 1 node with 4 cpu per node with maxtime by default (5 minutes) ------'
+echo '------- mpirun -np 4 ./myapp --------'
+mpirun -np 4 ./myapp
+
+echo '------- mpirun -np 1 -ppn 1 -maxtime 2 ./myapp --------'
+mpirun -np 1 -ppn 1 -maxtime 2 ./myapp
+
+echo '------- mpirun -np 2 -ppn 1 -maxtime 2 ./myapp --------'
+mpirun -np 2 -ppn 2 -maxtime 2 ./myapp
+
+echo '------- Task list: mps ---------'
+mps
+
+echo '------- Task list: pult t ---------'
+pult t
+
+echo '------- Status of executed task: mqtest myapp.1 --------'
+mqtest myapp.1
+mqtest myapp.2
+mqtest myapp.3
+echo '------- End of run.sh ------'
