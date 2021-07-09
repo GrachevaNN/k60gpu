739ed9af3dec3389154f13b553dbec610563b819
GrachevaNN <grann72@mail.ru>
2021-07-09 09:02:59 +0300

    09.07.2021_ 9:02:59,52

--- lab02/run.sh
+++ lab02/run.sh
@@ -2,7 +2,7 @@ git pull
 
 echo ''
 echo '------- Compiling main.c into main.o: mpicxx -O3 -c main.cpp ------'
-mpicxx -O3 -c main.cpp
+mpicxx -O3 -std=c++11 -c main.cpp
 echo '-------- mpicxx -o myapp2 main.o -----'
 mpicxx -o myapp2 main.o
 echo '-------------'
