3b96b212dd2395f244534d9b86ddc6ad53122a1a
GrachevaNN <grann72@mail.ru>
2021-07-13 14:45:17 +0300

    13.07.2021_14:45:17,95

--- lab03/clear.sh
+++ lab03/clear.sh
@@ -2,10 +2,6 @@ clear
 
 echo '------ Stopping task myapp3.1: mcancel myapp3.1 -------'
 mcancel myapp3.1
-echo '------ Stopping task myapp3.2: mcancel myapp3.2 -------'
-mcancel myapp3.2
-echo '------ Stopping task myapp3.3: mcancel myapp3.3 -------'
-mcancel myapp3.3
 
 echo '------ Removing temporary files -------'
 rm -r myapp3.*
--- lab03/run.sh
+++ lab03/run.sh
@@ -1,6 +1,5 @@
 git pull
 
-cat description.md
 echo ''
 echo '------- Compiling main.c into main.o: mpicxx -O3 -c main.cpp ------'
 mpicxx -O3 -std=c++11 -c main.cpp
@@ -12,12 +11,8 @@ echo '-------- mpicxx -o myapp3 main.o utils.o cpuThreads.o-----'
 mpicxx -L/usr/local/cuda/lib64 -lcudart -lm -o myapp3 main.o utils.o cpuThreads.o gpu.o
 echo '-------------'
 
-#echo '------- Starting myapp3 in 1 node with 1 cpu per node with maxtime by 2 minutes ------'
 echo '------- mpirun -np 1 -ppn 1 -maxtime 2 ./myapp3 --------'
 mpirun -np 1 -ppn 1 -maxtime 2 ./myapp3
-
-#echo '------- mpirun -np 2 -ppn 1 -maxtime 2 ./myapp --------'
-#mpirun -np 2 -ppn 1 -maxtime 2 ./myapp
 
 echo '------- Task list: mps ---------'
 mps
@@ -27,6 +22,4 @@ pult t
 
 echo '------- Status of executed task: mqtest myapp3.1 --------'
 mqtest myapp3.1
-#mqtest myapp3.2
-#mqtest myapp3.3
 echo '------- End of run.sh ------'
