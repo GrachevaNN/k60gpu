cce844d2910756f1e759f762161be7e20bf8da8f
GrachevaNN <grann72@mail.ru>
2021-07-14 13:53:27 +0300

    14-07-2021

--- lab04/run.sh
+++ lab04/run.sh
@@ -1,6 +1,5 @@
 git pull
 
-cat description.md
 echo ''
 echo '------- Compiling main.c into main.o: mpicxx -O3 -c main.cpp ------'
 mpicxx -O3 -std=c++11 -c main.cpp
@@ -12,12 +11,8 @@ echo '-------- mpicxx -o myapp4 main.o utils.o cpuThreads.o gpu.o-----'
 mpicxx -L/usr/local/cuda/lib64 -lcudart -lm -o myapp4 main.o utils.o cpuThreads.o gpu.o
 echo '-------------'
 
-#echo '------- Starting myapp4 in 1 node with 1 cpu per node with maxtime by 2 minutes ------'
 echo '------- mpirun -np 1 -ppn 1 -maxtime 2 ./myapp4 --------'
 mpirun -np 1 -ppn 1 -maxtime 2 ./myapp4
-
-#echo '------- mpirun -np 2 -ppn 1 -maxtime 2 ./myapp4 --------'
-#mpirun -np 2 -ppn 1 -maxtime 2 ./myapp4
 
 echo '------- Task list: mps ---------'
 mps
@@ -27,6 +22,5 @@ pult t
 
 echo '------- Status of executed task: mqtest myapp4.1 --------'
 mqtest myapp4.1
-#mqtest myapp4.2
-#mqtest myapp4.3
+
 echo '------- End of run.sh ------'
